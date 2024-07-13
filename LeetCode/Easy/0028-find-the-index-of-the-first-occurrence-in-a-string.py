2eba1e9c6d8c26b73b802c0198d9316bc3463fd4
binibin-dev <binibin.dev@gmail.com>
2024-07-13 22:46:12 +0900

    Time: 34 ms (64.91%), Space: 16.6 MB (21.67%) - LeetHub

---
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,19 @@
+# 문제
+# haystack 에서 needle 이 처음 발생하는 인덱스를 반환
+# haystack 에 needle 이 없다면 -1을 반환
+
+# 풀이
+# haystack 의 첫 글자부터 needle의 글자수만큼 비교
+# 
+# 인덱스가 넘어가지 않도록 함
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if haystack == needle:
+            return 0
+
+        len_needle = len(needle)
+        for i in range(len(haystack)-len_needle+1):
+            if haystack[i:i+len_needle] == needle:
+                return i
+        return -1
