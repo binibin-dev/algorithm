afb6f603f361e9005d888d55b11e9ad0ba1ed426
binibin-dev <binibin.dev@gmail.com>
2024-07-15 07:58:42 +0900

    Time: 56 ms (23.08%), Space: 17.4 MB (19.01%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram.py
@@ -4,22 +4,8 @@
 # anagram 이란 기존 문자를 정확히 한 번 사용하여 재배열해서 만들어진 다른 단어나 문장
 
 # 풀이
-# s 와 t 는 길이가 동일해야 함
-# 두 문자열을 순서대로 정렬하여 비교 횟수를 줄이도록 함
-# t 의 각 문자가 s 에 포함된다면 하나씩 제거, 없으면 바로 False를 반환
+# 두 문자열을 순서대로 정렬한 값이 같으면 anagram
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        s = sorted(s)
-        t = sorted(t)
-
-        for c in t:
-            if c in s:
-                s.remove(c)
-            else:
-                return False
-
-        return True
+        return sorted(s) == sorted(t)
