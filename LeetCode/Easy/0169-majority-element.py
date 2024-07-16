59163935faddba5c17e60fd39bbe63a2cd211699
binibin-dev <binibin.dev@gmail.com>
2024-07-16 09:35:27 +0900

    Time: 170 ms (59.45%), Space: 18.1 MB (14.3%) - LeetHub

---
+++ LeetCode/Easy/0169-majority-element.py
@@ -0,0 +1,15 @@
+# 문제
+# array 에서 과반수[사이즈/2]를 차지하는 요소를 반환
+# 배열에 항상 과반수를 차지하고 있는 요소가 존재한다고 가정한다.
+
+# 풀이
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d = {n: 0 for n in nums}
+        half = len(nums)//2
+
+        for n in nums:
+            d[n] += 1
+            if d[n] > half:
+                return n
