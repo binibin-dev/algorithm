bcc6e15b26d99600debff855cae866faf8323f22
binibin-dev <binibin.dev@gmail.com>
2024-07-26 22:03:22 +0900

    Time: 27 ms (94.16%), Space: 16.6 MB (13.40%) - LeetHub

--- LeetCode/Easy/0144-binary-tree-preorder-traversal.py
+++ LeetCode/Easy/0144-binary-tree-preorder-traversal.py
@@ -1,6 +1,12 @@
 # 문제
 # 이진 트리의 root 가 주어짐
-# 선위 순회한 결과를 반환
+# 전위 순회한 결과를 반환
+
+# 풀이
+# 노드 자신을 traversal 에 삽입
+# 노드의 왼쪽 자식을 전위 순회 (1부터 반복)
+# 노드의 오른쪽 자식을 전위 순회 (1부터 반복)
+# 모두 끝나면 traversal 반환
 
 # Definition for a binary tree node.
 # class TreeNode:
