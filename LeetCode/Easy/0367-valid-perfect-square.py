3bff819b83460e91e556fd170a1da696b6c53458
binibin-dev <binibin.dev@gmail.com>
2024-07-27 23:12:56 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Easy/0367-valid-perfect-square.py
@@ -0,0 +1,23 @@
+# 문제
+# 주어진 양의 정수가 완전 제곱인지 여부를 반환
+# 완전 제곱이란 어떤 정수의 제곱으로 표현될 수 있는 수
+
+# 풀이
+# 1부터 num 까지의 정수 배열을 만듦
+# 
+
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left, right = 1, num
+
+        while left < right:
+            mid = (left + right) // 2
+            if mid*mid < num: # 오른쪽 범위를 탐색해야 함
+                left = mid + 1
+            elif mid*mid > num: # 왼쪽 범위를 탐색해야 함
+                right = mid
+            else:
+                return True
+        
+        return False
