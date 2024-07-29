b94d006556631be185bfbf41aa91a18ead37727d
binibin-dev <binibin.dev@gmail.com>
2024-07-29 23:27:31 +0900

    Time: 29 ms (91.38%), Space: 16.4 MB (85.46%) - LeetHub

--- LeetCode/Easy/0263-ugly-number.py
+++ LeetCode/Easy/0263-ugly-number.py
@@ -3,10 +3,23 @@
 
 # 풀이
 # 소인수란 약수 중 소수인 수를 말함
+# 약수는 어떤 수로 나눴을 때 나머지가 0, 소수는 2 이상의 수로 나눠지지 않음
+# 정수 n 의 모든 약수는 쌍을 이루며, 약수 쌍 중 하나는 반드시 루트 n 보다 작음
+# 
+
+# 에라토스테네의 체
+# 1. 2부터 n까지 존재하는 모든 자연수를 나열
+# 2. 가장 작은 수를 x 로 지정
+# 3. x 의 배수를 모두 제거 (x는 그대로 둠)
+# 4. 2와 3을 반복
+
+import math
 
 class Solution:
     def isUgly(self, n: int) -> bool:
-        for i in range(2, n):
-            if n % i == 0 and i not in [2, 3, 5]:
-                return False
-        return True
+        if n <= 0: 
+            return False
+        for i in [2,3,5]:
+            while n % i == 0:
+                n = n // i
+        return n == 1 # 2, 3, 5 만으로 나눠져 1이 되면 True
