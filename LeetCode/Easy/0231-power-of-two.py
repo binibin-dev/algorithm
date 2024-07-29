b4949b1ad90b397b6a4e30ce794b2b766b759878
binibin-dev <binibin.dev@gmail.com>
2024-07-29 23:41:45 +0900

    Time: 33 ms (76.56%), Space: 16.6 MB (24.95%) - LeetHub

---
+++ LeetCode/Easy/0231-power-of-two.py
@@ -0,0 +1,15 @@
+# 문제 
+# 주어진 수가 2 의 거듭제곱이라면 True 를 반환
+
+# 문제
+# 2로 반복하여 나눠 1이 되면 2의 거듭제곱임
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 1:
+            return True
+        elif n == 0:
+            return False
+        while n % 2 == 0:
+            n = n // 2
+        return n == 1
