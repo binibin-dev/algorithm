d4801db584de2af4befe5df5cdd11efc8d9f3af3
binibin-dev <binibin.dev@gmail.com>
2024-08-06 22:27:58 +0900

    Time: 2394 ms (5.12%), Space: 19.9 MB (70.38%) - LeetHub

---
+++ LeetCode/Easy/0303-range-sum-query-immutable.py
@@ -0,0 +1,16 @@
+# 문제
+# 정수 배열 nums 과 그에 대한 쿼리를 처리
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def sumRange(self, left: int, right: int) -> int:
+        return sum([self.nums[i] for i in range(left, right+1)])
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
