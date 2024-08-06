255e7c87ea8e0eb6cfa997b98bb5336bbfce9dbd
binibin-dev <binibin.dev@gmail.com>
2024-08-06 22:29:56 +0900

    Time: 838 ms (17.63%), Space: 19.9 MB (70.38%) - LeetHub

--- LeetCode/Easy/0303-range-sum-query-immutable.py
+++ LeetCode/Easy/0303-range-sum-query-immutable.py
@@ -8,7 +8,8 @@ class NumArray:
         self.nums = nums
 
     def sumRange(self, left: int, right: int) -> int:
-        return sum([self.nums[i] for i in range(left, right+1)])
+        # return sum([self.nums[i] for i in range(left, right+1)])
+        return sum(self.nums[left:right+1])
 
 
 # Your NumArray object will be instantiated and called as such:
